d47b74586e2f37b048c471f96a7d44d97dfbacc4
unknown <hoanghuywww10@gmail.com>
2022-02-25 07:45:41 +0700

    Updates

---
+++ precache-manifest.4e4d2d0949add6cc8c788cf2b4fd4561.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b08e14191237834a4285e949a2fabd95",
+    "url": "/HoangHuyKG/WL.git/index.html"
+  },
+  {
+    "revision": "78e8fbe50569f15d895e",
+    "url": "/HoangHuyKG/WL.git/static/css/main.fc5dfaa2.chunk.css"
+  },
+  {
+    "revision": "55907a7cb973491e0047",
+    "url": "/HoangHuyKG/WL.git/static/js/2.b7df0148.chunk.js"
+  },
+  {
+    "revision": "a90bb751b102ed9d8f9443a23ededd32",
+    "url": "/HoangHuyKG/WL.git/static/js/2.b7df0148.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3d58b8aa31344748448",
+    "url": "/HoangHuyKG/WL.git/static/js/3.fc39b07c.chunk.js"
+  },
+  {
+    "revision": "78e8fbe50569f15d895e",
+    "url": "/HoangHuyKG/WL.git/static/js/main.de510be4.chunk.js"
+  },
+  {
+    "revision": "73ab1cbad9cb55a7df8d",
+    "url": "/HoangHuyKG/WL.git/static/js/runtime-main.ec81bd08.js"
+  }
+]);
